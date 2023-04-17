75105672f07a69318e3f30dc03a70ac3d1258ff7
Brett <NeonStar.Dev@Outlook.com>
2023-04-17 00:46:49 +0000

    fix: no more errors printed in restart script

--- homebase-restart.sh
+++ homebase-restart.sh
@@ -1,10 +1,9 @@
 #Check for updates to HOMEBASE repo
 git pull
 
-#If Homebase website is running, stop it and prune the system
-docker stop HOMEBASE-WEB
-docker system prune -f
+#If Homebase website is running, stop it
+docker stop HOMEBASE
 
 #Build and Run homebase website from Dockerfile
-docker build -t homebase /home/brett/HOMEBASE/website # Build Dockerfile for HOMEBASE
-docker run -d --name HOMEBASE-WEB --rm -p 80:80 homebase # Run HOMEBASE image
+docker build -t homebase:latest /home/brett/HOMEBASE/website # Build Dockerfile for HOMEBASE
+docker run -d --name HOMEBASE --rm -p 80:80 homebase # Run HOMEBASE image
