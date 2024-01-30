19ccd19ae9865a8234b3a4e585e6a647ce128f82
GitPunk23 <neonstar.developer@outlook.com>
2024-01-29 19:49:31 -0500

    update

---
+++ website/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Stop the existing Apache container
+docker-compose down
+
+# Pull the latest changes from GitHub
+git pull origin main
+
+# Start the Apache container
+docker-compose up -d
