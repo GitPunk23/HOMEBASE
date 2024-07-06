e3dba588a5184f2a7e7a5971a2ffc7a8e80edccf
GitPunk23 <103542010+GitPunk23@users.noreply.github.com>
2024-07-06 14:44:19 -0400

    fix script

--- CICD/deploy.sh
+++ CICD/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo touch hi
+
 # Stop the existing Apache container
-#docker-compose down
+docker-compose down
 
 # Pull the latest changes from GitHub
-#git pull origin main
+git pull origin main
 
 # Start the Apache container
-#docker-compose up -d
+docker-compose up -d
